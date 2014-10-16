8f5a06a21531a023d59868ad88c92e1c9c5af045
John Reese <jreese@fb.com>
2014-10-15 19:15:52 -0700

    Build communi-desktop from git

--- communi-desktop.rb
+++ communi-desktop.rb
@@ -2,14 +2,14 @@ require "formula"
 
 class CommuniDesktop < Formula
   homepage "https://github.com/communi/communi-desktop"
-  url "https://github.com/communi/communi-desktop/archive/v3.2.0.tar.gz"
-  sha1 "316696a50d658b6443ed8c1118c67ceea96b222d"
+  head "https://github.com/communi/communi-desktop.git"
 
   depends_on "qt5" => :build
   depends_on "libcommuni"
 
   def install
     ENV.deparallelize  # if your formula fails when building in parallel
+    system "git", "submodule", "update", "--init", "--recursive"
     system "qmake"
     system "make", "install"
   end
