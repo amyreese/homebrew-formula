f17821a50bb57b6ead7a9933916b34887aec2421
John Reese <john@noswap.com>
2015-11-30 14:04:08 -0800

    Build HEAD from jreese/libcommuni:mac-ssl

--- libcommuni.rb
+++ libcommuni.rb
@@ -3,7 +3,7 @@ require "formula"
 class Libcommuni < Formula
   homepage "https://github.com/communi/libcommuni"
   url "https://github.com/communi/libcommuni.git", :tag => "v3.4.0"
-  head "https://github.com/communi/libcommuni.git"
+  head "https://github.com/jreese/libcommuni.git", :branch => "mac-ssl"
 
   depends_on "qt5" => :build
 
