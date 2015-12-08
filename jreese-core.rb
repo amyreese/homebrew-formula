7189c8198d1a0e07e8cab4ba818fa268709227e4
John Reese <jreese@fb.com>
2015-12-08 13:54:30 -0800

    Update jreese-core, brew audit

--- jreese-core.rb
+++ jreese-core.rb
@@ -2,20 +2,20 @@ require "formula"
 
 class JreeseCore < Formula
   homepage "https://github.com/jreese/homebrew-formula"
-  url "https://github.com/jreese/hang", :tag => "v1.0"
+  url "https://github.com/jreese/hang.git", :tag => "v1.0", :revision => "b986fd0722a37600a2f4b38b1156e4cbc143748e"
 
   depends_on "ack"
   depends_on "aria2"
-  depends_on "autoconf"
-  depends_on "automake"
-  depends_on "cv"
+  depends_on "autoconf" => :run
+  depends_on "automake" => :run
+  depends_on "cask"
   depends_on "gcc"
-  depends_on "git"
+  depends_on :hg
   depends_on "htop-osx"
   depends_on "keybase"
-  depends_on "mercurial"
-  depends_on "mosh"
+  depends_on "mobile-shell"
   depends_on "node"
+  depends_on "progress"
   depends_on "python"
   depends_on "python3"
   depends_on "tmux"
