4aa8b849d3259c3e9d98d37993a57e8354ca000c
John Reese <john@noswap.com>
2017-02-05 20:38:07 -0800

    More dependencies in jreese-core

--- jreese-core.rb
+++ jreese-core.rb
@@ -5,24 +5,34 @@ class JreeseCore < Formula
   url "https://github.com/jreese/hang.git", :tag => "v1.1", :revision => "b348a37b7504bf33c12f7f0b580f09819d7d9bdb"
 
   depends_on "ack"
+  depends_on "android-platform-tools"
   depends_on "aria2"
   depends_on "autoconf" => :run
   depends_on "automake" => :run
   depends_on "cask"
+  depends_on "certbot"
+  depends_on "coreutils"
+  depends_on "gawk"
   depends_on "gcc"
+  depends_on "git"
   depends_on :hg
   depends_on "htop-osx"
   depends_on "keybase"
+  depends_on "keychain"
   depends_on "mobile-shell"
   depends_on "node"
+  depends_on "openssl"
+  depends_on "pkg-config"
   depends_on "progress"
   depends_on "python"
   depends_on "python3"
+  depends_on "renameutils"
   depends_on "tmux"
   depends_on "vim"
   depends_on "watch"
   depends_on "wget"
   depends_on "zsh"
+  depends_on "zstd"
 
   def install
     system "make", "PREFIX=#{prefix}", "install"
